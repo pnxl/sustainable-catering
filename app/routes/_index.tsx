9e5c819410c35221420651c5aa41862bc0350831
Jason <development@pnxl.dev>
2024-09-15 15:14:23 +0700

    styles: improve responsiveness

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,8 +7,8 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <main className="selection:bg-cyan-700 selection:text-white">
-      <div className="relative min-h-screen bg-[url('/img/naja-bertolt-jensen-BJUoZu0mpt0-unsplash.jpg')] bg-top bg-cover">
-        <div className="py-8 px-40">
+      <div className="relative justify-between flex flex-col min-h-screen bg-[url('/img/naja-bertolt-jensen-BJUoZu0mpt0-unsplash.jpg')] bg-top bg-cover">
+        <div className="py-8 px-8 md:px-24 lg:px-40">
           <header className="w-full flex flex-col gap-y-8 ">
             <img
               src="/logotype.svg"
@@ -18,10 +18,10 @@ export default function Index() {
             <hr className="border-2 border-black" />
           </header>
           <div className="text-center flex flex-col my-16">
-            <p className="font-body text-3xl font-bold italic text-black selection:bg-white selection:text-black">
+            <p className="font-body text-2xl sm:text-3xl font-bold italic text-black selection:bg-white selection:text-black">
               Take action
             </p>
-            <h1 className="font-display text-8xl font-bold leading-[1] text-black selection:bg-white selection:text-black">
+            <h1 className="font-display text-6xl sm:text-7xl md:text-8xl font-bold mt-4 md:mt-0 leading-[1] text-black selection:bg-white selection:text-black">
               For{" "}
               <span className="underline decoration-cyan-900 selection:bg-white selection:text-black">
                 sustainable
@@ -32,19 +32,19 @@ export default function Index() {
         </div>
         <img
           src="/svg/wave.svg"
-          className="w-full selection:bg-transparent"
+          className="w-full selection:bg-transparent bottom-0"
           alt=""
         />
       </div>
-      <div className="pt-16  bg-white relative">
-        <div className="text-center pb-24 px-40 flex flex-col gap-y-16 items-center">
-          <p className="font-body text-3xl font-bold italic text-black">
+      <div className="pt-16 bg-white relative">
+        <div className="text-center pb-24 px-8 md:px-24 lg:px-40 flex flex-col gap-y-16 items-center">
+          <p className="font-body text-2xl sm:text-3xl font-bold italic text-black">
             The Problem?
           </p>
-          <h1 className="font-display text-7xl  font-bold leading-[1] text-cyan-600 selection:bg-black selection:text-cyan-400">
+          <h1 className="font-display text-5xl sm:text-6xl md:text-7xl font-bold leading-[1] text-cyan-600 selection:bg-black selection:text-cyan-400">
             Single-use, non-biodegradable food containers.
           </h1>
-          <p className="font-body text-3xl text-black text-left w-4/5">
+          <p className="font-body text-2xl sm:text-3xl text-black text-left sm:w-4/5">
             Catering services use single-use plastic containers and disposable
             cutlery to serve food. While this is convenient — this practice
             contributes a significant amount to environmental pollution and
@@ -58,14 +58,14 @@ export default function Index() {
         />
       </div>
       <div className="pt-16 bg-cyan-700 relative">
-        <div className="text-center pb-24 px-40 flex flex-col gap-y-16 items-center">
-          <p className="font-body text-3xl font-bold italic text-white selection:text-cyan-700 selection:bg-white">
+        <div className="text-center pb-24 px-8 md:px-24 lg:px-40 flex flex-col gap-y-16 items-center">
+          <p className="font-body text-2xl sm:text-3xl font-bold italic text-white selection:text-cyan-700 selection:bg-white">
             The Solution?
           </p>
-          <h1 className="font-display text-7xl  font-bold leading-[1] text-white selection:bg-black selection:text-white">
+          <h1 className="font-display text-5xl sm:text-6xl md:text-7xl font-bold leading-[1] text-white selection:bg-black selection:text-white">
             Proper silverware and reusable, washable containers.
           </h1>
-          <p className="font-body text-3xl  text-white text-left w-4/5 selection:text-cyan-700 selection:bg-white">
+          <p className="font-body text-2xl sm:text-3xl text-white text-left sm:w-4/5 selection:text-cyan-700 selection:bg-white">
             Using food containers for catered meals that can be collected,
             washed, and reused for future meals.
             <br />
@@ -80,7 +80,7 @@ export default function Index() {
           alt=""
         />
       </div>
-      <footer className="bg-neutral-800 px-40 pb-24 flex flex-col gap-y-16">
+      <footer className="bg-neutral-800 px-8 md:px-24 lg:px-40 pb-24 flex flex-col gap-y-16">
         <div className="flex flex-row justify justify-between">
           <img
             src="/limantara-foundation.png"
@@ -95,7 +95,7 @@ export default function Index() {
         </div>
         <hr className="border-neutral-500" />
         <div className="flex flex-col gap-y-4">
-          <p className="font-body text-xl tracking-wide text-center">
+          <p className="font-body text-lg md:text-xl tracking-wide text-center">
             <span className="text-cyan-300 selection:bg-white selection:text-cyan-600">
               Sustainable Catering
             </span>{" "}
@@ -105,7 +105,7 @@ export default function Index() {
             </span>
             .
           </p>
-          <p className="font-body text-xl tracking-wide italic text-center opacity-75">
+          <p className="font-body text-lg md:text-xl tracking-wide italic text-center opacity-75">
             In support of{" "}
             <a
               href="https://www.breakfreefromplastic.org/"
