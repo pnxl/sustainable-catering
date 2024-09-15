0d854425dcf4503fd678f985aeb4895a088e8b0a
Jason <development@pnxl.dev>
2024-09-15 15:58:02 +0700

    fix: fonts css

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,8 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import tailwind from "~/tailwind.css?url";
+import fonts from "~/fonts.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,6 +21,8 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: tailwind },
+  { rel: "stylesheet", href: fonts },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
